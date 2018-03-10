2ba65f31d6d2aad302355f87d775488c83a07e7e
Bhupender Singh <bhupenders225@gmail.com>
2018-03-10 17:44:20 +0530

    updating animation
    creating animation to the popup

--- popup/popup.js
+++ popup/popup.js
@@ -4,82 +4,82 @@ var dataController = (function () {
         breed: [],
         information: [],
         age: [],
-        inoculations: [],
-        diseases: [],
-        parasites: []
+        lifeSpan: [],
+        height: [],
+        gender: []
     }*/
     var backup = [
         {
             name: "katrine",
-            breed: "pooch1",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur blandit condimentum. Proin accumsan lorem vel gra vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. Donec consectetur, diam in porta tempus, urna ligula ves tibulum nibh",
-            age: 1,
-            inoculations: "1none",
-            diseases: "1none",
-            parasites: "1none"
+            breed: "Chartreux",
+            information: "The Chartreux is a rare breed of domestic cat from France and is recognised by a number of registries around the world. The Chartreux is large and muscular (called cobby) with relatively short, fine-boned limbs, and very fast reflexes.",
+            age: "1 Year, 3 Months",
+            lifeSpan: "11-15 years",
+            height: "30cm",
+            gender: "Female"
         },
         {
             name: "Jennifer",
-            breed: "pooch2",
+            breed: "golden retriever pup",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur blandit condimentum. Proin accumsan lorem vel gra vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. Donec consectetur, diam in porta tempus, urna ligula ves tibulum nibh",
-            age: 2,
-            inoculations: "2none",
-            diseases: "2none",
-            parasites: "2none"
+            age: "2 Months",
+            lifeSpan: "10-12 years",
+            height: "25 cm",
+            gender: "male"
         },
         {
             name: "Woody",
-            breed: "pooch3",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur blandit condimentum. Proin accumsan lorem vel gra vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. Donec consectetur, diam in porta tempus, urna ligula ves tibulum nibh",
-            age: 3,
-            inoculations: "3none",
-            diseases: "3none",
-            parasites: "3none"
+            breed: "Golden Retriever",
+            information: "The Golden Retriever is a large-sized breed of dog bred as gun dogs to retrieve shot waterfowl such as ducks and upland game birds during hunting and shooting parties,[3] and were named 'retriever' because of their ability to retrieve shot game undamaged.",
+            age: "2 year",
+            lifeSpan: "10-12 years",
+            height: "59 cm",
+            gender: "Female"
         },
         {
             name: "Sophia",
-            breed: "pooch4",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur blandit condimentum. Proin accumsan lorem vel gra vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. Donec consectetur, diam in porta tempus, urna ligula ves tibulum nibh",
-            age: 4,
-            inoculations: "4none",
-            diseases: "4none",
-            parasites: "4none"
+            breed: "Harrier",
+            information: "The Harrier is similar to the English Foxhound, but smaller. Harriers stand between 21 and 24 inches at the shoulder, and adults weigh between 45 and 65 lbs. They do shed, have short hair and hanging ears, and come in a variety of color patterns.",
+            age: "5 Months",
+            lifeSpan: "12-15 year",
+            height: "35 cm",
+            gender: "Female"
         },
         {
             name: "Timmy",
-            breed: "pooch5",
+            breed: "scotish fold",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur blandit condimentum. Proin accumsan lorem vel gra vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. Donec consectetur, diam in porta tempus, urna ligula ves tibulum nibh",
-            age: 5,
-            inoculations: "5none",
-            diseases: "5none",
-            parasites: "5none"
+            age: "5 months",
+            lifeSpan: "13-15 years",
+            height: "22cm",
+            gender: "Male"
         },
         {
             name: "Charly",
-            breed: "pooch6",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur blandit condimentum. Proin accumsan lorem vel gra vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. Donec consectetur, diam in porta tempus, urna ligula ves tibulum nibh",
-            age: 6,
-            inoculations: "6none",
-            diseases: "6none",
-            parasites: "6none"
+            breed: "Beagle",
+            information: "The beagle is a breed of small hound that is similar in appearance to the much larger foxhound. The beagle is a scent hound, developed primarily for hunting hare. With a great sense of smell and superior tracking instinct, the beagle is employed as detection dog for prohibited agricultural imports and foodstuffs in quarantine around the world.",
+            age: "1 Year",
+            lifeSpan: "12-15 years",
+            height: "39 cm",
+            gender: "Male"
         },
         {
             name: "Scarlett",
-            breed: "pooch7",
+            breed: "lorem",
             information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur blandit condimentum. Proin accumsan lorem vel gra vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. Donec consectetur, diam in porta tempus, urna ligula ves tibulum nibh",
-            age: 7,
-            inoculations: "7none",
-            diseases: "7none",
-            parasites: "7none"
+            age: "3 year",
+            lifeSpan: "13 years",
+            height: "55 cm",
+            gender: "Female"
         },
         {
             name: "Freddie",
-            breed: "pooch8",
-            information: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce efficitur blandit condimentum. Proin accumsan lorem vel gra vida fringilla. Suspendisse potenti. Mauris ut pulvinar nunc. Donec consectetur, diam in porta tempus, urna ligula ves tibulum nibh",
-            age: 8,
-            inoculations: "8none",
-            diseases: "8none",
-            parasites: "8none"
+            breed: "Domestic Cat",
+            information: "Cats are similar in anatomy to the other felids, with a strong flexible body, quick reflexes, sharp retractable claws, and teeth adapted to killing small prey. Cat senses fit a crepuscular and predatory ecological niche.",
+            age: "8 months",
+            lifeSpan: "11-12 years",
+            height: "50 cm",
+            gender: "Female"
         }
     ]
 
@@ -92,9 +92,9 @@ var dataController = (function () {
                 breed: backup[position].breed,
                 information: backup[position].information,
                 age: backup[position].age,
-                inoculations: backup[position].inoculations,
-                diseases: backup[position].disease,
-                parasites: backup[position].parasites
+                lifeSpan: backup[position].lifeSpan,
+                height: backup[position].disease,
+                gender: backup[position].gender
             }
         },
 
@@ -115,36 +115,16 @@ var UIController = (function () {
         popup: "#popup"
     };
 
-    var updateUI = function (data) {/*
-        var html = '<section id="popup"><div class="popup_container"><div class="petimage"><img src=%image%></div><div class="pet_detail"><h4>%name%</h4><h6>%breed%</h6><p>%information%</p><ul type="none"><li><span>•</span>Age: %age%</li><li><span>•</span>Inoculations: %inoculations%</li><li><span>•</span>Diseases: %diseases%</li><li><span>•</span>Parasites: %parasites%</li></ul></div><span class="cross_button"><p>&#10005;</p></span></div></section>';
-
-        var newHtml = html.replace("%image%", data.image);
-        newHtml = newHtml.replace("%name%", data.name);
-        newHtml = newHtml.replace("%breed%", data.breed);
-        newHtml = newHtml.replace("%information%", data.information);
-        newHtml = newHtml.replace("%age%", data.age);
-        newHtml = newHtml.replace("%inoculations%", data.inoculations);
-        newHtml = newHtml.replace("%diseases%", data.diseases);
-        newHtml = newHtml.replace("%parasites%", data.parasites);
-
-        document.querySelector(DOMStrings.place).insertAdjacentHTML("afterend", newHtml);*/
+    var updateUI = function (data) {
         
         document.querySelector(".petimage img").src = data.image;
         document.querySelector(".pet_detail h4").textContent = data.name;
         document.querySelector(".pet_detail h6").textContent = data.breed;
         document.querySelector(".pet_detail p").textContent = data.information;
         document.querySelectorAll(".pet_detail ul li span ~ span")[0].textContent = data.age;
-        document.querySelectorAll(".pet_detail ul li span ~ span")[1].textContent = data.inoculations;
-        document.querySelectorAll(".pet_detail ul li span ~ span")[2].textContent = data.diseases;
-        document.querySelectorAll(".pet_detail ul li span ~ span")[3].textContent = data.parasites;
-        
-        
-        
-        document.querySelector("#popup").classList.add("popup_after");
-        document.querySelector(".popup_container").classList.add("popup_container_after");
-        
-        
-        console.log(data.name);
+        document.querySelectorAll(".pet_detail ul li span ~ span")[1].textContent = data.lifeSpan;
+        document.querySelectorAll(".pet_detail ul li span ~ span")[2].textContent = data.height;
+        document.querySelectorAll(".pet_detail ul li span ~ span")[3].textContent = data.gender;
         
     }
 
@@ -188,27 +168,25 @@ var controller = (function (dataCtrl, UICtrl) {
     var getTargetData = function (event) {
 
         var existID = event.target.parentNode.id;
-        console.log(existID !== "our_pet_content" && event.srcElement.textContent == document.querySelector(".pets button").textContent);
 
         if (existID !== "our_pet_content" && event.srcElement.textContent === document.querySelector(".pets button").textContent) {
             var id = event.target.parentNode.id;
             var image = event.target.parentNode.childNodes[1].childNodes[1].src;
 
             mergeObject(id, image);
-
-            /* to add event listners to newly created div called popup */
-            boot();
-
-
-
-
+            
+            document.querySelector("#popup").style.visibility = "visible";
+            document.querySelector(".popup_container").classList.add("show_popup");
+            
             // stop scroll
             stopScroll("enable");
         }
     }
 
     var removePopup = function (event) {
-        console.log(document.querySelector(DOM.body).removeChild(document.querySelector(DOM.popup)));
+        
+        document.querySelector("#popup").style.visibility = "hidden";
+        document.querySelector(".popup_container").classList.remove("show_popup");
 
         stopScroll("disable");
     }
